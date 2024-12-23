1995dcdff20034a12d20a5317590a4e25524d2bb
bulBBATTA <ahn3207@gmail.com>
2024-12-23 22:10:31 +0900

    뿌슝빠슝 첫 첫 커밋 우하하

---
+++ test.py
@@ -0,0 +1,9 @@
+# 사용자로부터 두 숫자 입력 받기
+num1 = float(input("첫 번째 숫자를 입력하세요: "))
+num2 = float(input("두 번째 숫자를 입력하세요: "))
+
+# 두 숫자의 합 계산
+sum_result = num1 + num2
+
+# 결과 출력
+print(f"두 숫자의 합은 {sum_result}입니다.")
